bf66199e629cdc7c657f649eb937d02407eccbbd
Moritz E. Beber <midnighter@posteo.net>
2024-01-02 17:48:14 +0100

    refactor: add value label to range input

--- src/components/ConfidenceRangeInput.svelte
+++ src/components/ConfidenceRangeInput.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
-    export let value: number = 3;
+    export let value: number = 2;
+
     const min = 1;
-    const max = 5;
+    const max = 3;
+    const range = {
+        1: 'Uncertain',
+        2: 'Somewhat Confident',
+        3: 'Highly Confident'
+    };
 </script>
 
-<div class="min-w-max w-1/2">
-    <span>Uncertain</span>
-    <input type="range" bind:value {min} {max} class="inline-block" />
-    <span>Highly Confident</span>
+<div class="w-40">
+    <label class="label">
+        <span>{range[value]}</span>
+        <input type="range" bind:value {min} {max} class="inline-block" />
+    </label>
 </div>
