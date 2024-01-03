8090647f29bb6a7450274a00f206f54dc4f573bd
Moritz E. Beber <midnighter@posteo.net>
2024-01-03 17:49:37 +0100

    fix: set deployed root path

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter({ fallback: 'index.html' }),
         alias: { $components: 'src/components' },
-        paths: { base: process.env.NODE_ENV === 'production' ? '/submit' : '' }
+        paths: { base: process.env.NODE_ENV === 'production' ? '/curator-form' : '' }
     }
 };
 
